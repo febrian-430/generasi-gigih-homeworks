a04845abb6963c5783477cc657bac9e1469af84b
febrian <febrian430@gmail.com>
2021-07-28 18:17:47 +0700

    refactor: create a function for each consonant and vowel character

--- module-4/session-2/simple_encoder.rb
+++ module-4/session-2/simple_encoder.rb
@@ -17,27 +17,35 @@ class SimpleEncoder
 
     def encode_char(character)
         if VOWEL_CHARS.include? character
-            ord = character.ord
-            new_ord = ord - VOWEL_ENCODER_NUM
-            if new_ord >= MIN_ORD
-                new_ord.chr
-            else
-                diff = MIN_ORD - new_ord
-                new_ord = MAX_ORD - diff + 1
-                new_ord.chr
-            end
+            return encode_vowel_char(character)
         elsif character == ''
             return ''
         else
-            ord = character.ord
-            new_ord = ord+CONSONANT_ENCODER_NUM
-            if new_ord <= MAX_ORD
-                new_ord.chr
-            else
-                diff = new_ord - MAX_ORD
-                new_ord = MIN_ORD + diff - 1
-                new_ord.chr
-            end
+            return encode_consonant_char(character)
+        end
+    end
+
+    def encode_vowel_char(character)
+        ord = character.ord
+        new_ord = ord - VOWEL_ENCODER_NUM
+        if new_ord >= MIN_ORD
+            new_ord.chr
+        else
+            diff = MIN_ORD - new_ord
+            new_ord = MAX_ORD - diff + 1
+            new_ord.chr
+        end
+    end
+
+    def encode_consonant_char(character)
+        ord = character.ord
+        new_ord = ord+CONSONANT_ENCODER_NUM
+        if new_ord <= MAX_ORD
+            new_ord.chr
+        else
+            diff = new_ord - MAX_ORD
+            new_ord = MIN_ORD + diff - 1
+            new_ord.chr
         end
     end
 end
