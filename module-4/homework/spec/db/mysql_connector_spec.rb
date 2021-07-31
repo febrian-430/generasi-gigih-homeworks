5c65e3ba65893f1633e59a05887c29a31222e25f
febrian <febrian430@gmail.com>
2021-07-31 21:16:37 +0700

    MySQLDB#transaction unit tests (no mock or stub)

--- module-4/homework/spec/db/mysql_connector_spec.rb
+++ module-4/homework/spec/db/mysql_connector_spec.rb
@@ -2,14 +2,25 @@ require 'rspec'
 require './db/mysql_connector.rb'
 
 describe MySQLDB do
+    #im not sure how to test this
+    #so i tested this with real db connection
     describe "#transaction" do
-        context "when containing a block that is invalid query statement" do
-            it 'should return true' do
+        context "when containing a block that is invalid (query) statement" do
+            it 'should return false' do
+                result = MySQLDB.transaction do |client|
+                    client.query("invalid query;")
+                    a = 1/0
+                end
+                expect(result).to eq(false)
             end
         end
 
-        context "when a block is valid query statement" do
-            it 'should return false' do
+        context "when all blocks are valid query statement" do
+            it 'should return true' do
+                result = MySQLDB.transaction do |client|
+                    client.query("select * from items;")
+                end
+                expect(result).to eq(true)
             end
         end
     end
