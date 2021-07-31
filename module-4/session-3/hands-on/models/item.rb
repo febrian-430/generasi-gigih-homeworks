447903af8856a0244f6aa1cd61c5d29e76826190
febrian <febrian430@gmail.com>
2021-07-31 13:20:19 +0700

    added unit tests for item model except for save and update

--- module-4/session-3/hands-on/models/item.rb
+++ module-4/session-3/hands-on/models/item.rb
@@ -3,7 +3,7 @@ require './db/mysql_connector.rb'
 
 class Item
     attr_accessor :name, :price, :id, :categories
-
+   
     def initialize(id, name, price)
         @id = id
         @name = name
@@ -11,15 +11,6 @@ class Item
         @categories = []
     end
 
-    def self.bind_to_items(raw)
-        items = []
-        raw.each do | data |
-            item = Item.new(data["id"], data["name"], data["price"])
-            items.push(item)
-        end
-        return items
-    end
-    
     
     def self.get_all_items
         client = MySQLDB.get_client
@@ -52,14 +43,14 @@ class Item
     
     def self.get_item_by_id(id)
         client = MySQLDB.get_client
-        raw = client.query("
-            select id, name, price
-            from items
-            where id = #{id}
-        ")
+        raw = client.query(
+            "select id, name, price
+             from items
+             where id = #{id}"
+             )
         items = bind_to_items(raw)
         
-        if items.length > 0
+        if !items.nil? && items.length > 0
             item = items[0] 
             raw = client.query("
                     select c.id, c.name from categories c 
@@ -77,22 +68,14 @@ class Item
 
     def self.filter_by_category(category_name)
         client = MySQLDB.get_client
-        raw = client.query("
-            select i.* from items i
+        raw = client.query(
+           "select i.* from items i
             join item_categories ic on ic.item_id = i.id 
             join categories c on c.id = ic.category_id
-            where c.name = '#{category_name}'
-        ")  
+            where c.name = '#{category_name}'"
+            )  
         items = bind_to_items(raw)
         return items
-    end
-
-    def self.last
-        client = MySQLDB.get_client
-        raw = client.query("
-            select * from items order by id desc limit 1
-        ")
-        return bind_to_items(raw)[0]
     end
     
     def save?
@@ -103,7 +86,6 @@ class Item
 
     def save
         return false unless self.save?
-        # client = MySQLDB.get_client
         success = MySQLDB.transaction do |client|
             result = client.query("INSERT INTO items(name, price) VALUES ('#{@name}', #{@price});")
             @id = client.last_id
@@ -115,26 +97,21 @@ class Item
                 end
                 insert_category_statement = "INSERT INTO item_categories(item_id, category_id) VALUES %s;" % bulk.join(",")
             end
-            puts @id
             client.query(insert_category_statement) unless !insert_category_statement
         end
-        
+        puts "save is #{success}"
         return success
-    end
-
-    def unlink_category
     end
     
     def update?
         return false if @id.nil?
-        return false if @name.nil?
+        return false if @name.nil? || @name.empty?
         return false if @price.nil?
         true
     end
 
     def update
         return false unless update?
-        client = MySQLDB.get_client
         
         current_category_ids = Category.of_item(self.id).map{ |category| category.id }
         updated_categories = self.categories&.map{ |category| category.id }
@@ -167,8 +144,8 @@ class Item
             delete_statement = "DELETE FROM item_categories WHERE item_id = #{self.id} AND category_id IN(%s);" % deleted_categories.join(",")
             puts delete_statement.inspect
         end
-        begin
-            client.query("START TRANSACTION;")
+
+        success = MySQLDB.transaction do |client|
             client.query("
                 UPDATE items 
                 SET name = '#{@name}', 
@@ -177,11 +154,6 @@ class Item
             ")
             client.query(create_statement) if !create_statement.nil?
             client.query(delete_statement) if !delete_statement.nil?
-            client.query("COMMIT;")
-            return true
-        rescue
-            client.query("ROLLBACK;")
-            return false
         end
     end
     
@@ -195,5 +167,16 @@ class Item
         client = MySQLDB.get_client
         client.query("DELETE FROM items WHERE id = #{@id}")
         return true
+    end
+
+    private 
+
+    def self.bind_to_items(raw)
+        items = []
+        raw.each do | data |
+            item = Item.new(data["id"], data["name"], data["price"])
+            items.push(item)
+        end
+        return items
     end
 end
